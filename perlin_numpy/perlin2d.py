7ce0e7facda7d86a842a6c81967c7964631759fd
AyrtonB <AyrtonBourn@Outlook.com>
2021-02-14 12:29:40 +0000

    setting seed only when not None

--- perlin_numpy/perlin2d.py
+++ perlin_numpy/perlin2d.py
@@ -2,19 +2,22 @@ import numpy as np
 
 from warnings import warn
 
+
 def interpolant(t):
-    return t*t*t*(t*(t*6 - 15) + 10)
-    
+    return t * t * t * (t * (t * 6 - 15) + 10)
+
 
 def check_res(res, shape):
     for dim, dim_res in enumerate(res):
-        assert shape[dim]%dim_res == 0, f'Dimension {dim} has a length of {shape[dim]} which can not be divided using the resolution specified: {dim_res}'
+        assert (
+            shape[dim] % dim_res == 0
+        ), f"Dimension {dim} has a length of {shape[dim]} which can not be divided using the resolution specified: {dim_res}"
 
     return
 
 
 def generate_perlin_noise_2d(
-        shape, res, tileable=(False, False), interpolant=interpolant, seed=None
+    shape, res, tileable=(False, False), interpolant=interpolant, seed=None
 ):
     """Generate a 2D numpy array of perlin noise.
 
@@ -40,46 +43,52 @@ def generate_perlin_noise_2d(
     delta = (res[0] / shape[0], res[1] / shape[1])
     d = (shape[0] // res[0], shape[1] // res[1])
 
-    grid = np.mgrid[0:res[0]:delta[0], 0:res[1]:delta[1]]\
-             .transpose(1, 2, 0) % 1
-    
+    grid = np.mgrid[0 : res[0] : delta[0], 0 : res[1] : delta[1]].transpose(1, 2, 0) % 1
+
     # Gradients
-    np.random.seed(seed)
-    angles = 2*np.pi*np.random.rand(res[0]+1, res[1]+1)
+    if seed is not None:
+        np.random.seed(seed)
+
+    angles = 2 * np.pi * np.random.rand(res[0] + 1, res[1] + 1)
     gradients = np.dstack((np.cos(angles), np.sin(angles)))
-    
+
     if tileable[0]:
-        gradients[-1,:] = gradients[0,:]
+        gradients[-1, :] = gradients[0, :]
     if tileable[1]:
-        gradients[:,-1] = gradients[:,0]
-    
+        gradients[:, -1] = gradients[:, 0]
+
     gradients = gradients.repeat(d[0], 0).repeat(d[1], 1)
 
-    g00 = gradients[    :-d[0],    :-d[1]]
-    g10 = gradients[d[0]:     ,    :-d[1]]
-    g01 = gradients[    :-d[0],d[1]:     ]
-    g11 = gradients[d[0]:     ,d[1]:     ]
-    
+    g00 = gradients[: -d[0], : -d[1]]
+    g10 = gradients[d[0] :, : -d[1]]
+    g01 = gradients[: -d[0], d[1] :]
+    g11 = gradients[d[0] :, d[1] :]
+
     # Ramps
-    n00 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]  )) * g00, 2)
-    n10 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]  )) * g10, 2)
-    n01 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]-1)) * g01, 2)
-    n11 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]-1)) * g11, 2)
-    
+    n00 = np.sum(np.dstack((grid[:, :, 0], grid[:, :, 1])) * g00, 2)
+    n10 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1])) * g10, 2)
+    n01 = np.sum(np.dstack((grid[:, :, 0], grid[:, :, 1] - 1)) * g01, 2)
+    n11 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1] - 1)) * g11, 2)
+
     # Interpolation
     t = interpolant(grid)
-    n0 = n00*(1-t[:,:,0]) + t[:,:,0]*n10
-    n1 = n01*(1-t[:,:,0]) + t[:,:,0]*n11
+    n0 = n00 * (1 - t[:, :, 0]) + t[:, :, 0] * n10
+    n1 = n01 * (1 - t[:, :, 0]) + t[:, :, 0] * n11
 
-    noise = np.sqrt(2)*((1-t[:,:,1])*n0 + t[:,:,1]*n1)
-    
+    noise = np.sqrt(2) * ((1 - t[:, :, 1]) * n0 + t[:, :, 1] * n1)
+
     return noise
 
 
 def generate_fractal_noise_2d(
-        shape, res, octaves=1, persistence=0.5,
-        lacunarity=2, tileable=(False, False),
-        interpolant=interpolant, seed=None
+    shape,
+    res,
+    octaves=1,
+    persistence=0.5,
+    lacunarity=2,
+    tileable=(False, False),
+    interpolant=interpolant,
+    seed=None,
 ):
     """Generate a 2D numpy array of fractal noise.
 
@@ -108,14 +117,14 @@ def generate_fractal_noise_2d(
     noise = np.zeros(shape)
     frequency = 1
     amplitude = 1
-    
+
     for _ in range(octaves):
         octave_noise = generate_perlin_noise_2d(
-            shape, (frequency*res[0], frequency*res[1]), tileable, interpolant, seed
+            shape, (frequency * res[0], frequency * res[1]), tileable, interpolant, seed
         )
 
         noise += amplitude * octave_noise
         frequency *= lacunarity
         amplitude *= persistence
-    
+
     return noise
