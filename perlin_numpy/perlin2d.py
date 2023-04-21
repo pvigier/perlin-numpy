a29c29ecefef6770ce005b9ac186de916755aaa2
David Li <dli7319@gmail.com>
2023-04-21 11:33:10 -0400

    Enable arbitrary resolution in 2D noise

--- perlin_numpy/perlin2d.py
+++ perlin_numpy/perlin2d.py
@@ -14,8 +14,7 @@ def generate_perlin_noise_2d(
         shape: The shape of the generated array (tuple of two ints).
             This must be a multple of res.
         res: The number of periods of noise to generate along each
-            axis (tuple of two ints). Note shape must be a multiple of
-            res.
+            axis (tuple of two ints).
         tileable: If the noise should be tileable along each axis
             (tuple of two bools). Defaults to (False, False).
         interpolant: The interpolation function, defaults to
@@ -27,10 +26,6 @@ def generate_perlin_noise_2d(
     Raises:
         ValueError: If shape is not a multiple of res.
     """
-    delta = (res[0] / shape[0], res[1] / shape[1])
-    d = (shape[0] // res[0], shape[1] // res[1])
-    grid = np.mgrid[0:res[0]:delta[0], 0:res[1]:delta[1]]\
-             .transpose(1, 2, 0) % 1
     # Gradients
     angles = 2*np.pi*np.random.rand(res[0]+1, res[1]+1)
     gradients = np.dstack((np.cos(angles), np.sin(angles)))
@@ -38,18 +33,24 @@ def generate_perlin_noise_2d(
         gradients[-1,:] = gradients[0,:]
     if tileable[1]:
         gradients[:,-1] = gradients[:,0]
-    gradients = gradients.repeat(d[0], 0).repeat(d[1], 1)
-    g00 = gradients[    :-d[0],    :-d[1]]
-    g10 = gradients[d[0]:     ,    :-d[1]]
-    g01 = gradients[    :-d[0],d[1]:     ]
-    g11 = gradients[d[0]:     ,d[1]:     ]
+    grid = np.stack(np.meshgrid(
+        np.arange(0, shape[1]) * res[1] / shape[1],
+        np.arange(0, shape[0]) * res[0] / shape[0],
+    )[::-1], axis=-1)
+    grid_floor = np.floor(grid).astype(int)
+    grid_ceil = np.ceil(grid).astype(int)
+    g00 = gradients[grid_floor[:,:,0], grid_floor[:,:,1]]
+    g10 = gradients[grid_ceil[:,:,0], grid_floor[:,:,1]]
+    g01 = gradients[grid_floor[:,:,0], grid_ceil[:,:,1]]
+    g11 = gradients[grid_ceil[:,:,0], grid_ceil[:,:,1]]
+    grid_frac = grid - grid_floor
     # Ramps
-    n00 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]  )) * g00, 2)
-    n10 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]  )) * g10, 2)
-    n01 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]-1)) * g01, 2)
-    n11 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]-1)) * g11, 2)
+    n00 = np.sum(np.dstack((grid_frac[:,:,0]  , grid_frac[:,:,1]  )) * g00, 2)
+    n10 = np.sum(np.dstack((grid_frac[:,:,0]-1, grid_frac[:,:,1]  )) * g10, 2)
+    n01 = np.sum(np.dstack((grid_frac[:,:,0]  , grid_frac[:,:,1]-1)) * g01, 2)
+    n11 = np.sum(np.dstack((grid_frac[:,:,0]-1, grid_frac[:,:,1]-1)) * g11, 2)
     # Interpolation
-    t = interpolant(grid)
+    t = interpolant(grid_frac)
     n0 = n00*(1-t[:,:,0]) + t[:,:,0]*n10
     n1 = n01*(1-t[:,:,0]) + t[:,:,0]*n11
     return np.sqrt(2)*((1-t[:,:,1])*n0 + t[:,:,1]*n1)
